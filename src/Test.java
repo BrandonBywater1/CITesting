fb1512b2f9e5d64cf5560afff8c692594891e27e
b7043159 <b7043159@newcastle.ac.uk>
2021-03-06 17:11:46 +0000

    test

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(addTwo(5, 1));
+    }
+
+    public static int addTwo(int n1, int n2){
+        return n1 + n2;
+    }
+}
